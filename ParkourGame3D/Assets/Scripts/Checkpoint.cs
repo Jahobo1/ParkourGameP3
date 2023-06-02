1db209c45d18568edc736a5d7a4daa3577f76932
Jahobo1 <116302023+Jahobo1@users.noreply.github.com>
2023-06-02 11:02:34 -0700

    donme
    checkpoint fixed

--- ParkourGame3D/Assets/Scripts/Checkpoint.cs
+++ ParkourGame3D/Assets/Scripts/Checkpoint.cs
@@ -12,13 +12,13 @@ public class CheckPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.y < -dead)
+        if (player.transform.position.y < -dead)
         {
             player.transform.position = vectorPoint;
         }
